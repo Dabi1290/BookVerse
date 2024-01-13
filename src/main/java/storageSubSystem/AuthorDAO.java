e632dabfaee6bfe03a6c4c9741aa9a019e66ce19
Tonaion02 <antonioambrosiopub@gmail.com>
2024-01-13 17:41:23 +0100

    Find authors by email

--- src/main/java/storageSubSystem/AuthorDAO.java
+++ src/main/java/storageSubSystem/AuthorDAO.java
@@ -1,6 +1,7 @@
 package storageSubSystem;
 
 import userManager.Author;
+import userManager.User;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -8,6 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class AuthorDAO {
     private DataSource ds=null;
@@ -31,5 +34,37 @@ public class AuthorDAO {
         }
 
         return a;
+    }
+
+    public Set<User> findAuthorsByEmail(String email) throws SQLException {
+
+        String query = "SELECT * FROM Author, User WHERE userId_fk=User.id AND email LIKE ?";
+
+        Connection c = ds.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(query);
+        ps.setString(1, email);
+        ResultSet rs = ps.executeQuery();
+
+        Set<User> authors = new TreeSet<>();
+
+        while(rs.next()) {
+            String name = rs.getString("name");
+            String surname = rs.getString("surname");
+            String email_ = rs.getString("email");
+            String password = rs.getString("password");
+            int id = rs.getInt("User.id");
+
+            User user = new User();
+            user.setId(id);
+            user.setName(name);
+            user.setSurname(surname);
+            user.setEmail(email_);
+            user.setPassword(password);
+
+            authors.add(user);
+        }
+
+        return authors;
     }
 }
