ae8cbcde614a9e5f5ac9ce39480dda959d2cc1b4
Dabi1290 <davide.amoruso33@gmail.com>
2024-01-14 21:39:09 +0100

    fill db fix and user dao

--- db/fillDb.sql
+++ db/fillDb.sql
@@ -1,8 +1,8 @@
 INSERT INTO Genre(name) values ("genere1");
 INSERT INTO Genre(name) values ("genere2");
 
-INSERT INTO User(email, password, name, surname) values("email", "password", "antonio", "ambrosio");
-INSERT INTO User(email, password, name, surname) values("email2", "password", "antonio2", "ambrosio2");
+INSERT INTO User(email, password, name, surname) values("email", "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86", "antonio", "ambrosio");
+INSERT INTO User(email, password, name, surname) values("email2", "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86", "antonio2", "ambrosio2");
 
 INSERT INTO Validator(id) values (1);
 INSERT INTO Author(id) values (1);
