196099d3855b2fb16dc0d1cba2dfc832d6a20355
Dabi1290 <davide.amoruso33@gmail.com>
2024-01-20 00:03:30 +0100

    add dependencies for testing

---
+++ src/test/java/proposalManager/ProposalTest.java
@@ -0,0 +1,49 @@
+package proposalManager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProposalTest {
+
+    @Test
+    void makeProposal() {
+    }
+
+    @Test
+    void isValidParameter() {
+    }
+
+    @Test
+    void approve() {
+    }
+
+    @Test
+    void refuse() {
+
+    }
+
+    @Test
+    void permanentlyRefuse() {
+    }
+
+    @Test
+    void pay() {
+    }
+
+    @Test
+    void correct() {
+    }
+
+    @Test
+    void assignValidator() {
+    }
+
+    @Test
+    void addVersion() {
+    }
+
+    @Test
+    void lastVersion() {
+    }
+}
